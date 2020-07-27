42f4bb94209d226e06505e8034e659ce5b18e5b4
ricsonl <ricsonluizv@gmail.com>
2020-07-26 23:44:58 -0300

    build

--- README.md
+++
@@ -1,1 +0,0 @@
-# ricsonl.github.io
---
+++ precache-manifest.8ae3f816d8b33a83b30282963996b436.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "763d43d8209bdbdc56dc9d165cdc7a11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1db83b5d40db257ab64",
+    "url": "/static/css/main.68d54b87.chunk.css"
+  },
+  {
+    "revision": "88525d02268e795f16e1",
+    "url": "/static/js/2.6ccbfa9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6ccbfa9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1db83b5d40db257ab64",
+    "url": "/static/js/main.0d7dfb1a.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  }
+]);
